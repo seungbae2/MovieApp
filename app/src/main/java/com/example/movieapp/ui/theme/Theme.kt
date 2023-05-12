6cd9ea1808cd2c196403adcb51eee6fcdedb3a19
SB <seungbae2@gmail.com>
2023-05-12 23:41:19 +0900

    Theme Colors

--- app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
@@ -2,43 +2,31 @@ package com.example.movieapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.example.movieapp.ui.theme.color.*
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+private val LocalColors = staticCompositionLocalOf { ColorSet.Red.LightColors }
 
 @Composable
-fun MovieAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun MovieAppTheme(
+    myColors: ColorSet,
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        myColors.DarkColors
     } else {
-        LightColorPalette
+        myColors.LightColors
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalColors provides colors ) {
+        MaterialTheme(
+            colors = colors.material,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
