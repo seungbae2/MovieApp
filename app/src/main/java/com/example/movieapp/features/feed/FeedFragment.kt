45d13debd38022d40becf97074b0956e25d9a3c1
SB <seungbae2@gmail.com>
2023-05-18 22:03:18 +0900

    Fragment Setting

---
+++ app/src/main/java/com/example/movieapp/features/feed/FeedFragment.kt
@@ -0,0 +1,28 @@
+package com.example.movieapp.features.feed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.movieapp.ui.theme.MovieAppTheme
+
+class FeedFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MovieAppTheme {
+                    Text("FeedFragment")
+                }
+            }
+        }
+    }
+
+}
