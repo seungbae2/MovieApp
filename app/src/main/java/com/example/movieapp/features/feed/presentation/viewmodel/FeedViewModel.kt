8aab1c1b56cbd128f4952a5806fd7ec0f97ee11e
SB <seungbae2@gmail.com>
2023-05-23 20:33:44 +0900

    Retrofit Api Module add

---
+++ app/src/main/java/com/example/movieapp/features/feed/presentation/viewmodel/FeedViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.movieapp.features.feed.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class FeedViewModel (
+
+) : ViewModel(){
+}
