b4a20b8eeddfd664109f8844140558d03c65e74d
SB <seungbae2@gmail.com>
2023-05-25 20:56:32 +0900

    ViewModel StateFlow, SharedFlow 설정

---
+++ app/src/main/java/com/example/movieapp/features/feed/presentation/output/FeedState.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp.features.feed.presentation.output
+
+sealed class FeedState {
+    object Loading: FeedState()
+
+    class Main(
+        val movieList = List<MovieFeedItemEntity>
+    ): FeedState()
+
+    class Failed(
+        val reason: String
+    ): FeedState()
+}
