a81c673d5795e9854ace33e21ff61468cab895a6
SB <seungbae2@gmail.com>
2023-05-30 21:18:20 +0900

    DetailUseCase, DetailViewModel

--- app/src/main/java/com/example/movieapp/features/detail/DetailFragment.kt
+++ app/src/main/java/com/example/movieapp/features/detail/DetailFragment.kt
@@ -8,13 +8,16 @@ import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.example.movieapp.features.detail.presentation.viewmodel.MovieDetailViewModel
 import com.example.movieapp.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DetailFragment: Fragment() {
-//    private val viewModel: MovieDetailViewModel by viewModels()
+    private val viewModel: MovieDetailViewModel by viewModels()
     private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -34,6 +37,9 @@ class DetailFragment: Fragment() {
     }
 
     private fun init() {
-
+        lifecycleScope.launch {
+            viewModel.initMovieName(args.movieName)
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/movieapp/features/detail/presentation/output/MovieDetailState.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.features.detail.presentation.output
+
+import com.example.movieapp.features.common.entity.MovieDetailEntity
+
+sealed class MovieDetailState {
+    object Initial : MovieDetailState()
+    class Main(val movieDetailEntity: MovieDetailEntity) : MovieDetailState()
+}
