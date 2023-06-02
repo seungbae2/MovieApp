658eee76abc6679a97fead2f9840c52b0ed35d8f
SB <seungbae2@gmail.com>
2023-06-02 23:16:51 +0900

    MovieDetailScreen add to DetailFragment observeUiEffects() add

--- app/src/main/java/com/example/movieapp/features/detail/DetailFragment.kt
+++ app/src/main/java/com/example/movieapp/features/detail/DetailFragment.kt
@@ -5,14 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.movieapp.features.detail.presentation.MovieDetailScreen
+import com.example.movieapp.features.detail.presentation.output.DetailUiEffect
 import com.example.movieapp.features.detail.presentation.viewmodel.MovieDetailViewModel
+import com.example.movieapp.ui.navigation.safeNavigate
 import com.example.movieapp.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -25,12 +33,16 @@ class DetailFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        observeUiEffects()
         init()
 
         return ComposeView(requireContext()).apply {
             setContent {
                 MovieAppTheme {
-                    Text("FeedFragment")
+                    MovieDetailScreen(
+                        movieDetailState = viewModel.outputs.detailState.collectAsState(),
+                        input = viewModel.inputs
+                    )
                 }
             }
         }
@@ -41,5 +53,34 @@ class DetailFragment: Fragment() {
             viewModel.initMovieName(args.movieName)
         }
     }
+    private fun observeUiEffects() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.outputs.detailUiEffect.collectLatest {
+                    when (it) {
+                        is DetailUiEffect.Back -> {
+                            findNavController().navigateUp()
+                        }
 
+                        is DetailUiEffect.OpenUrl -> {
+//                            findNavController().safeNavigate(
+//                                DetailFragmentDirections.actionDetailToImdbDialog(
+//                                    it.url
+//                                )
+//                            )
+                        }
+
+                        is DetailUiEffect.RateMovie -> {
+//                            findNavController().safeNavigate(
+//                                DetailFragmentDirections.actionDetailToRating(
+//                                    movieName = it.movieTitle,
+//                                    rating = it.rating
+//                                )
+//                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
